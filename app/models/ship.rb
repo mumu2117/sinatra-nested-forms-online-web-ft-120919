4cb8cb2355c6806d568cda7da32d8780914f1cdf
Jennifer Miller <jmillerarcher@me.com>
2020-02-05 19:43:22 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+  
+  attr_reader :name, :type, :booty
+
+  @@ships = []
+
+  def initialize(info)
+    @name = info[:name]
+    @type = info[:type]
+    @booty = info[:booty]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships.clear
+  end
+  
 end
